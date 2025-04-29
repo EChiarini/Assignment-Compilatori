bade9296b994418399f0731e578cc51533a7aeec
EChiarini <320558@studenti.unimore.it>
2025-04-29 12:01:05 +0200

    Update FakeLICM.sh

--- Assignment_3/test/FakeLICM.sh
+++ Assignment_3/test/FakeLICM.sh
@@ -1,3 +1,7 @@
+clang -Xclang -disable-O0-optnone -S -emit-llvm -O0 verifyLICM.c -o verifyLICM.ll
+opt -p mem2reg verifyLICM.ll -o loop.bc
+llvm-dis loop.bc -o loop.ll
+
 make -C ../build/
 opt -load-pass-plugin ../build/libFakeLICM.so -p fake-licm loop.ll -o loop.bc
 llvm-dis loop.bc -o loopLICM.ll
